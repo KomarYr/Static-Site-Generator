106afbae3f705922a2bd1c25c9b27aa4d79a2649
KomarYr <komar.yr@gmail.com>
2026-06-13 14:38:32 +0100

    Creating Leaf Node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,29 +1,39 @@
-
-
-
 class HTMLNode:
     def __init__(
             self, 
             tag: str | None = None, 
             value: str | None = None, 
             children: list | None = None,
-            props: dict | None = None
-        ):
+            props: dict[str, str] | None = None
+        ) -> None:
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
 
-
     def to_html(self):
-        raise NotImplementedError
-
+        raise NotImplementedError("to_html method not implemented")
 
     def props_to_html(self):
         if isinstance(self.props, dict):
-            return " ".join(f' {key}="{value}"' for key, value in self.props.items())
+            return " " + " ".join(f'{key}="{value}"' for key, value in self.props.items())
         return ""
 
-
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str | None, value: str, props: dict[str, str] | None = None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("leaf nodes must have a value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -7,7 +7,7 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode()
         self.assertEqual("", node.props_to_html())
         self.assertEqual(
-                "HTMLNode(None, None, None, None)", repr(node))
+                "HTMLNode(None, None, children: None, None)", repr(node))
 
 
     def test_without_value(self):
@@ -15,11 +15,44 @@ class TestHTMLNode(unittest.TestCase):
         self.assertIsNone(node.value)
         self.assertIsInstance(node.children, HTMLNode)
 
+
     def test_without_children(self):
         node = HTMLNode(tag="div", value="hello")
         self.assertIsNone(node.children)
         self.assertIsNotNone(node.value)
 
+
+    def test_to_html_props(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
+
+    def test_leaf_invalid_value(self):
+        node = LeafNode("p", None)
+        # Call .to_html() only inside with block
+        with self.assertRaises(ValueError) as msg:
+            node.to_html()
+            
+        self.assertEqual(str(msg.exception), "leaf nodes must have a value")
+        
+    def test_leaf_empty_tag(self):
+        node = LeafNode(None, "only raw text")
+        self.assertEqual(node.to_html(), "only raw text")
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
 
 
 if __name__ == "__main__":
