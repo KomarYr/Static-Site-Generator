26406ef70603d875a93713e94b9d2d4fdc87efd5
KomarYr <komar.yr@gmail.com>
2026-06-21 17:58:46 +0100

    Imlement copy the content from source directory to destination directory(static to public)

---
+++ src/copy_content.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+
+
+
+def copy_content(src: str, dst: str, cleaned: bool = False) -> None:
+    if not cleaned:
+        shutil.rmtree(dst)
+        cleaned = True
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+    #print(f"dst created, here content: {os.listdir(dst)}")
+
+    src_content = os.listdir(src)
+    for item in src_content:
+        src_path = os.path.join(src, item)
+        
+        if os.path.isfile(src_path):
+            #print(f"this is a file: {src_path}")
+            shutil.copy(src_path, dst)
+        else:
+            #print(f"this is folder in {src}: {src_path}")
+            copy_content(src_path, os.path.join(dst, item), cleaned)
+                
+
+
+if __name__ == "__main__":
+    print(copy_content(".."))
+
--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 from textnode import TextNode, TextType
+from copy_content import copy_content
 
 
 def main():
     text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
 
-    print(text_node)
-
+    #print(text_node)
+    source_directory = "./static"
+    destination_directory = "./public"
+    copy_content(source_directory, destination_directory)
 
 
 
